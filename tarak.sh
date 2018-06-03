f104260a6402fe1462bb8468b86ec84b51834499
tarak mama <tarak@email.com>
2018-06-03 15:58:49 +0530

    tarak.sh file is created

---
+++ tarak.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#################################
+#purpose: to learn shell scripts
+#version:1.0
+#owner:tarak<tarak@gmail.com>
+#input:none
+#output:printe some msg on screen
+##################################
+#print welcome msg
+echo "welcome tarak"
+sleep 10 #wait for ten sec
+echo "the end but welcome"
