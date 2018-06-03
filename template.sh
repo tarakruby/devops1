7c5efb2955dfc0c3eeceb7f668ce208951da0b2b
tarak mama <tarak@email.com>
2018-06-03 15:54:32 +0530

    my template

---
+++ template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#################################
+#purpose: to learn shell scripts
+#version:1.0
+#owner:tarak<tarak@gmail.com>
+#input:none
+#output:none
+##################################
